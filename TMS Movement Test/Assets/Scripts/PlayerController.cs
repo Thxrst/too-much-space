a05fe9fc65676fc39026fb2ba370f15b00585f5d
Kyle Hadley <75557816+blackymagick@users.noreply.github.com>
2023-04-06 23:34:33 -0700

    Added new Art and adjusted player script
    >Added more tile maps
    >refractor the player script with a case statement. pretty nice. The previous script code is still available

--- TMS Movement Test/Assets/Scripts/PlayerController.cs
+++ TMS Movement Test/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rb;
-    [SerializeField] float spd;
+    
+    [SerializeField] float speed;
     [SerializeField] float thrust;
     [SerializeField] LayerMask Ground;
-    private int lrValue;
-    //string nextAction = "left";
-    private int nextAction = 0;
-    private bool pressSpace;
-    private bool hasJump = true;
+    [SerializeField] private int lrValue;
+    [SerializeField] private int nextAction = 0;
+    [SerializeField] private int jumpNum;
+    [SerializeField] private bool hasJump = true;
     private BoxCollider2D coll;
+    private Rigidbody2D rb;
 
-    // float [] nextAction = {"left","jump1","right","jump2"};
 
     void Start()
     {
@@ -27,54 +26,116 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-            if (nextAction == 0 && Input.GetKeyDown("space"))
-            {
-                lrValue = -1;
-                nextAction++;
-                Debug.Log("left");
-            }
-                
-            else if (nextAction == 2 && Input.GetKeyDown("space"))
-            {
-                lrValue = 1;
-                nextAction++;
-                Debug.Log("right");
-            }
+       
+        //playermovementOne();
+        playerMovementTwo();
 
-
-            else if (hasJump)
-            {
-                if (nextAction == 3 && Input.GetKeyDown("space"))
-                {
-                    lrValue = 0;
-                    rb.AddForce(transform.up * thrust, ForceMode2D.Impulse);
-                    hasJump = false;
-                    nextAction++;
-                    Debug.Log("jump2");
-                }
-
-                if (nextAction == 1 && Input.GetKeyDown("space"))
-                {
-                    lrValue = 0;
-                    rb.AddForce(transform.up * thrust, ForceMode2D.Impulse);
-                    hasJump = false;
-                    nextAction++;
-                    Debug.Log("jump1");
-                }
-            }
-
-        if (nextAction > 3)
-        {
-            nextAction = 0;
-        }
-
-        hasJump = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, Ground); // boolcheck for grounded
+        // boolcheck for grounded
+        
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(lrValue * (100 * spd) * Time.deltaTime, rb.velocity.y);
+        hasJump = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, Ground);
+        rb.velocity = new Vector2(lrValue * (100 * speed) * Time.deltaTime, rb.velocity.y);
     }
 
+    private int jumpCheck()
+    {
+        if (hasJump)
+        {
+            jumpNum = 1;
+            Debug.Log(jumpNum);
+        }
+            
+        return jumpNum;
+    }
+
+
+    void playermovementOne()
+    {
+        if (nextAction == 0 && Input.GetKeyDown("space"))
+        {
+            lrValue = -1;
+            nextAction++;
+            Debug.Log("left");
+        }
+
+        else if (nextAction == 2 && Input.GetKeyDown("space"))
+        {
+            lrValue = 1;
+            nextAction++;
+            Debug.Log("right");
+        }
+
+        else if (jumpCheck() == 1)
+        {
+            if (nextAction == 3 && Input.GetKeyDown("space"))
+            {
+                lrValue = 0;
+                rb.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+                hasJump = false;
+                jumpNum = 0;
+                nextAction = 0;
+                Debug.Log("jump2");
+            }
+
+            if (nextAction == 1 && Input.GetKeyDown("space"))
+            {
+                lrValue = 0;
+                rb.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+                jumpNum = 0;
+                nextAction++;
+                Debug.Log("jump1");
+            }
+        }
+    }
+
+    //Refractored the code to be in a case statement style. see if you like this? It's first step to an event system.
+    //little more robust as we can call mutilple functions / scripts in a clean manner rather than have a bunch of
+    //if statements 
+    void playerMovementTwo()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            switch(nextAction)
+            {
+                case 0:
+                    Move(-1); // int variable to push character to left
+                    jumpCheck(); //check for jumpCache only when grounded;
+                    break;
+                case 1:
+                    Jump(); //jump function and leading to next action.
+                    nextAction++;
+                    break;
+                case 2:
+                    Move(1); // int variable to push character to right
+                    jumpCheck(); //check for jumpCache only when grounded;
+                    break;
+                case 3:
+                    Jump(); // jump function leading back to 0
+                    nextAction = 0;
+                    break;
+            }
+        }
+    }
+
+    void Jump()
+    {
+        if (jumpCheck() == 1)
+        {
+            lrValue = 0;
+            rb.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+            jumpNum = 0;
+            hasJump = false;
+        }
+    }
+
+    void Move(int direction)
+    {
+        lrValue = direction;
+        nextAction++;
+        
+    }
 
 }
