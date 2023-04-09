dff2497745331e311f30326e7ebc70ec709f97c9
Thxrst <thomassteil3@gmail.com>
2023-04-09 16:11:32 -0500

    Objective Sound

--- TMS Movement Test/Assets/Scripts/Objective.cs
+++ TMS Movement Test/Assets/Scripts/Objective.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class Objective : MonoBehaviour
 {
 
+    //Audio
+    [SerializeField] private EventReference objectiveSound;
+    //Audio
 
     ParticleSystem ps;
     SpriteRenderer sr;
@@ -17,10 +21,13 @@ public class Objective : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.tag == "Player")
+        if (col.gameObject.tag == "Player" && sr.enabled == true)
         {
             sr.enabled = false;
             ps.Play();
+            //Audio
+            AudioManager.instance.PlayOneShot(objectiveSound, this.transform.position);
+            //Audio
             Debug.Log("Objective/Player Contact");
         }
     }
